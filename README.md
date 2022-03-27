d78c9ca88f23ee9eff1cb33258b0741e92444e7f
cyberkid05 <cyberadithya05@gmail.com>
2022-03-27 10:03:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,285 @@
-# PythonProjects
+# PyDoSc - Passive Google Dork (OSint) and Scrap
+
+## Introduction
+
+`pydosc` automates Google searching for potentially vulnerable web pages and applications on the Internet.  It replaces
+manually performing Google dork searches with a web GUI browser.
+
+There are 2 parts.  The first is `ghdb_scraper.py` that retrieves the latest Google dorks and the second portion is
+`pydosc.py` that leverages the information gathered by `ghdb_scraper.py`.
+
+The core Google search library now uses the more flexible [yagooglesearch](https://github.com/opsdisk/yagooglesearch)
+instead of [googlesearch](https://github.com/MarioVilas/googlesearch).  Check out the
+[yagooglesearch README](https://github.com/opsdisk/yagooglesearch/blob/master/README.md) for a more in-depth explanation
+of the library differences and capabilities.
+
+This version of `pydosc` also supports native HTTP(S) and SOCKS5 application support, so no more wrapping it in a tool
+like `proxychains4` if you need proxy support.  You can specify multiple proxies to use in a round-robin fashion by
+providing a comma separated string of proxies using the `-p` switch.
+
+## What are Google dorks?
+
+Offensive Security maintains the Google Hacking Database (GHDB) found here:
+<https://www.exploit-db.com/google-hacking-database>.  It is a collection of Google searches, called dorks, that can be
+used to find potentially vulnerable boxes or other juicy info that is picked up by Google's search bots.
+
+## Terms and Conditions
+
+The terms and conditions for `pydosc` are the same terms and conditions found in
+[yagooglesearch](https://github.com/opsdisk/yagooglesearch#terms-and-conditions).
+
+This code is supplied as-is and you are fully responsible for how it is used.  Scraping Google Search results may
+violate their [Terms of Service](https://policies.google.com/terms).  Another Python Google search library had some
+interesting information/discussion on it:
+
+* [Original issue](https://github.com/aviaryan/python-gsearch/issues/1)
+* [A response](https://github.com/aviaryan/python-gsearch/issues/1#issuecomment-365581431>)
+* Author created a separate [Terms and Conditions](https://github.com/aviaryan/python-gsearch/blob/master/T_AND_C.md)
+* ...that contained link to this [blog](https://benbernardblog.com/web-scraping-and-crawling-are-perfectly-legal-right/)
+
+Google's preferred method is to use their [API](https://developers.google.com/custom-search/v1/overview).
+
+## Installation
+
+Scripts are written for Python 3.6+.  Clone the git repository and install the requirements.
+
+```bash
+git clone https://github.com/opsdisk/pydosc.git
+cd pydosc
+virtualenv -p python3.7 .venv  # If using a virtual environment.
+source .venv/bin/activate  # If using a virtual environment.
+pip install -r requirements.txt
+```
+
+## ghdb_scraper.py
+
+To start off, `pydosc.py` needs a list of all the current Google dorks.  The repo contains a `dorks/` directory with
+the current dorks when the `ghdb_scraper.py` was last run. It's advised to run `ghdb_scraper.py` to get the freshest
+data before running `pydosc.py`.  The `dorks/` directory contains:
+
+* the `all_google_dorks.txt` file which contains all the Google dorks, one per line
+* the `all_google_dorks.json` file which is the JSON response from GHDB
+* Individual category dorks
+
+Dork categories:
+
+```python
+categories = {
+    1: "Footholds",
+    2: "File Containing Usernames",
+    3: "Sensitives Directories",
+    4: "Web Server Detection",
+    5: "Vulnerable Files",
+    6: "Vulnerable Servers",
+    7: "Error Messages",
+    8: "File Containing Juicy Info",
+    9: "File Containing Passwords",
+    10: "Sensitive Online Shopping Info",
+    11: "Network or Vulnerability Data",
+    12: "Pages Containing Login Portals",
+    13: "Various Online devices",
+    14: "Advisories and Vulnerabilities",
+}
+```
+
+### Using ghdb_scraper.py as a script
+
+Write all dorks to `all_google_dorks.txt`, `all_google_dorks.json`, and individual categories if you want more
+contextual data about each dork.
+
+```bash
+python ghdb_scraper.py -s -j -i
+```
+
+### Using ghdb_scraper as a module
+
+The `ghdb_scraper.retrieve_google_dorks()` function returns a dictionary with the following data structure:
+
+```python
+ghdb_dict = {
+    "total_dorks": total_dorks,
+    "extracted_dorks": extracted_dorks,
+    "category_dict": category_dict,
+}
+```
+
+Using a Python shell (like `python` or `ipython`) to explore the data:
+
+```python
+import ghdb_scraper
+
+dorks = ghdb_scraper.retrieve_google_dorks(save_all_dorks_to_file=True)
+dorks.keys()
+dorks["total_dorks"]
+
+dorks["extracted_dorks"]
+
+dorks["category_dict"].keys()
+
+dorks["category_dict"][1]["category_name"]
+```
+
+## <span>pydosc.py</span>
+
+### Using <span>pydosc.py</span> as a script
+
+```bash
+python pydosc.py -d example.com -g dorks.txt 
+```
+
+### Using pydosc as a module
+
+The `pydosc.pydosc.go()` function returns a dictionary with the data structure below (dorks used are made up examples):
+
+```python
+{
+    "dorks": {
+        "inurl:admin": {
+            "urls_size": 3,
+            "urls": [
+                "https://github.com/marmelab/ng-admin",
+                "https://github.com/settings/admin",
+                "https://github.com/akveo/ngx-admin",
+            ],
+        },
+        "inurl:gist": {
+            "urls_size": 3,
+            "urls": [
+                "https://gist.github.com/",
+                "https://gist.github.com/index",
+                "https://github.com/defunkt/gist",
+            ],
+        },
+    },
+    "initiation_timestamp": "2021-08-27T11:35:30.638705",
+    "completion_timestamp": "2021-08-27T11:36:42.349035",
+}
+```
+
+Using a Python shell (like `python` or `ipython`) to explore the data:
+
+```python
+import pydosc
+
+pg = pydosc.pydosc(
+    google_dorks_file="dorks.txt",
+    domain="github.com",
+    max_search_result_urls_to_return_per_dork=3,
+    save_pydosc_results_to_json_file=True,
+    save_urls_to_file=True,
+    verbosity=5,
+)
+pydosc_results_dict = pg.go()
+
+pydosc_results_dict.keys()
+
+pydosc_results_dict["initiation_timestamp"]
+
+pydosc_results_dict["completion_timestamp"]
+
+for key,value in pydosc_results_dict["dorks"].items():
+    print(f"dork: {key}")
+    for url in value["urls"]:
+        print(url)
+```
+
+## Tuning Results
+
+## Scope to a specific domain
+
+The `-d` switch can be used to scope the results to a specific domain and functions as the Google search operator:
+
+```none
+site:github.com
+```
+
+### Wait time between Google dork searchers
+
+* `-i` - Specify the **minimum** delay between dork searches, in seconds.  Don't make this too small, or your IP will
+get HTTP 429'd quickly.
+* `-x` - Specify the **maximum** delay between dork searches, in seconds.  Don't make this too big or the searches will
+take a long time.
+
+The values provided by `-i` and `-x` are used to generate a list of 20 randomly wait times, that are randomly selected
+between each different Google dork search.
+
+### Number of results to return
+
+`-m` - The total max search results to return per Google dork.  Each Google search request can pull back at most 100
+results at a time, so if you pick `-m 500`, 5 separate search queries will have to be made for each Google dork search,
+which will increase the amount of time to complete.
+
+## Google is blocking me!
+
+Performing 7300+ search requests to Google as fast as possible will simply not work.  Google will rightfully detect it
+as a bot and block your IP for a set period of time.  One solution is to use a bank of HTTP(S)/SOCKS proxies and pass
+them to `pydosc`
+
+### Native proxy support
+
+Pass a comma separated string of proxies to `pydosc` using the `-p` switch.
+
+```bash
+python pydosc.py -g dorks.txt -p http://myproxy:8080,socks5h://127.0.0.1:9050,socks5h://127.0.0.1:9051
+```
+
+You could even decrease the `-i` and `-x` values because you will be leveraging different proxy IPs.  The proxies passed
+to `pydosc` are selected by round robin.
+
+### proxychains4 support
+
+Another solution is to use `proxychains4` to round robin the lookups.
+
+Install `proxychains4`
+
+```bash
+apt install proxychains4 -y
+```
+
+Edit the `/etc/proxychains4.conf` configuration file to round robin the look ups through different proxy servers.  In
+the example below, 2 different dynamic socks proxies have been set up with different local listening ports (9050 and
+9051).
+
+```bash
+vim /etc/proxychains4.conf
+```
+
+```ini
+round_robin
+chain_len = 1
+proxy_dns
+remote_dns_subnet 224
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+[ProxyList]
+socks4 127.0.0.1 9050
+socks4 127.0.0.1 9051
+```
+
+Throw `proxychains4` in front of the `pydosc.py` script and each *request* lookup will go through a different proxy (and
+thus source from a different IP).
+
+```bash
+proxychains4 python pydosc.py -g dorks/all_google_dorks.txt -o -s
+```
+
+Note that this may not appear natural to Google if you:
+
+1) Simulate "browsing" to `google.com` from IP #1
+2) Make the first search query from IP #2
+3) Simulate clicking "Next" to make the second search query from IP #3
+4) Simulate clicking "Next to make the third search query from IP #1
+
+For that reason, using the built in `-p` proxy support is preferred because, as stated in the `yagooglesearch`
+documentation, the "provided proxy is used for the entire life cycle of the search to make it look more human, instead
+of rotating through various proxies for different portions of the search."
+
+## License
+
+Distributed under the GNU General Public License v3.0. See [LICENSE](./LICENSE) for more information.
+
+## Contact
+
+[@opsdisk](https://twitter.com/opsdisk)
+
+Project Link: [https://github.com/opsdisk/pydosc](https://github.com/opsdisk/pydosc)
---
+++ pydosc.py
@@ -0,0 +1,413 @@
+#!/usr/bin/env python
+
+# Standard Python libraries.
+# modules = ["argparse", "datetime", "json", "logging", "os", "random", "re", "sys", "time",]
+# for module in modules:
+#     exec("import {}".format(module))
+import argparse
+import datetime
+import json
+import logging
+import os
+import random
+import re
+import sys
+import time
+
+# Third party Python libraries.
+import yagooglesearch
+
+# Custom Python libraries.
+
+__version__ = "1.0.0"
+
+banner = '''
+o--o           o-o            o-o        
+|   |          |  \          |            
+O--o    o  o   |   O   o-o    o-o     o-o 
+|       |  |   |  /    | |       |   |    
+o       o--O   o-o     o-o   o--o     o-o 
+           |                              
+        o--o                                
+'''
+
+print(banner)
+
+# Logging
+ROOT_LOGGER = logging.getLogger("pydosc")
+# ISO 8601 datetime format by default.
+LOG_FORMATTER = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)s] %(message)s")
+
+# Setup file logging.
+log_file_handler = logging.FileHandler("pydosc.py.log")
+log_file_handler.setFormatter(LOG_FORMATTER)
+ROOT_LOGGER.addHandler(log_file_handler)
+
+# Setup console logging.
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LOG_FORMATTER)
+ROOT_LOGGER.addHandler(console_handler)
+
+
+class pydosc:
+    """pydosc class object"""
+
+    def __init__(
+        self,
+        google_dorks_file,
+        domain="",
+        # max_urls_per_dork=100,
+        max_urls_per_dork=1,
+        save_pydosc_results_to_json_file=False,
+        proxies="",
+        save_urls_to_file=False,
+        min_delay_secs=37,
+        max_delay_secs=60,
+        disable_verify_ssl=False,
+        verbosity=4,
+    ):
+        """Initialize pydosc class object."""
+
+        # Run parameter checks.
+        if not os.path.exists(google_dorks_file):
+            print("Specify a valid file containing Google dorks with -g")
+            sys.exit(0)
+
+        if min_delay_secs < 0:
+            print("Minimum delay between dork searches (-i) must be greater than 0")
+            sys.exit(0)
+
+        if max_delay_secs < 0:
+            print("max_delay_secs (-x) must be greater than 0")
+            sys.exit(0)
+
+        if max_delay_secs <= min_delay_secs:
+            print(
+                "max_delay_secs (-x) must be greater than "
+                "min_delay_secs (-i)"
+            )
+            sys.exit(0)
+
+        if max_urls_per_dork < 0:
+            print("max_urls_per_dork (-m) must be greater than 0")
+            sys.exit(0)
+
+        # All passed paramters look good, assign to the class object.
+        self.google_dorks_file = google_dorks_file
+        self.google_dorks = []
+        # with open(google_dorks_file, "r") as fh:
+        with open(google_dorks_file, "r", encoding='utf-8') as fh:
+            for line in fh.read().splitlines():
+                if line.strip():
+                    self.google_dorks.append(line)
+        self.domain = domain
+        self.max_urls_per_dork = max_urls_per_dork  # maximum search result urls to return per dork
+        self.save_pydosc_results_to_json_file = save_pydosc_results_to_json_file
+        self.proxies = proxies.strip().strip(",").split(",")
+        self.save_urls_to_file = save_urls_to_file
+        self.min_delay_secs = min_delay_secs    # minimum delay between dork searches in seconds
+        self.max_delay_secs = max_delay_secs    # maximum delay between dork searches in seconds
+        self.disable_verify_ssl = disable_verify_ssl
+        self.verbosity = verbosity
+
+        # Fancy way of generating a list of 20 random values between min_delay_secs and
+        # max_delay_secs.  A random value is selected between each different Google
+        # dork search.
+        """
+        1) Generate a random list of values between min_delay_secs and
+           max_delay_secs
+        2) Round those values to the tenths place
+        3) Re-case as a list
+        4) Sort the list
+        """
+        self.delay_between_dork_searches_list = sorted(
+            list(
+                map(
+                    lambda x: round(x, 1),
+                    [
+                        random.uniform(
+                            min_delay_secs,
+                            max_delay_secs,
+                        )
+                        for _ in range(20)
+                    ],
+                )
+            )
+        )
+
+        # self.base_file_name = f'pydosc_results_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}'
+        self.base_file_name = 'my_osint_links'
+        self.total_urls_found = 0
+        self.proxy_rotation_index = 0
+
+        # Assign log level.
+        ROOT_LOGGER.setLevel((6 - self.verbosity) * 10)
+
+    def go(self):
+        """Start pydosc Google dork search."""
+
+        initiation_timestamp = datetime.datetime.now().isoformat()
+
+        ROOT_LOGGER.info(f"Initiation timestamp: {initiation_timestamp}")
+
+        # Initialize starting dork number.
+        dork_counter = 1
+
+        total_dorks_to_search = len(self.google_dorks)
+
+        # Initialize dictionary to track dork results.
+        self.pydosc_results_dict = {
+            "dorks": {},
+            "initiation_timestamp": initiation_timestamp,
+            "completion_timestamp": "",
+        }
+
+        for dork in self.google_dorks:
+
+            # fmt: off
+            self.pydosc_results_dict["dorks"][dork] = {
+                "urls_size": 0,
+                "urls": [],
+
+            }
+            # fmt: on
+
+            try:
+                dork = dork.strip()
+
+                # Search for the URLs to collect.
+                if self.domain:
+                    query = f"site:{self.domain} {dork}"
+                else:
+                    query = dork
+
+                """
+                Google search web GUI message for large search string queries:
+                    "the" (and any subsequent words) was ignored because we limit queries to 32 words.
+                """
+                # Search string is longer than 32 words.
+                if len(query.split(" ")) > 32:
+                    ignored_string = " ".join(query.split(" ")[32:])
+                    ROOT_LOGGER.warning(
+                        "Google limits queries to 32 words (separated by spaces):  Removing from search query: "
+                        f"'{ignored_string}'"
+                    )
+
+                    # Update query variable.
+                    updated_query = " ".join(query.split(" ")[0:32])
+
+                    # If original query is in quotes, append a double quote to new truncated updated_query.
+                    if query.endswith('"'):
+                        updated_query = f'{updated_query}"'
+
+                    ROOT_LOGGER.info(f"New search query: {updated_query}")
+
+                    query = updated_query
+
+                # Rotate through the list of proxies using modulus to ensure the index is in the self.proxies list.
+                proxy_index = self.proxy_rotation_index % len(self.proxies)
+                proxy = self.proxies[proxy_index]
+                self.proxy_rotation_index += 1
+
+                # Instantiate a new yagooglesearch.SearchClient object for each Google dork.
+                client = yagooglesearch.SearchClient(
+                    query,
+                    tbs="li:1",  # Verbatim search.
+                    # num=100,  # Retrieve up to 100 Google search results at time.
+                    # Max desired valid URLs to collect per dork.
+                    num = 10,
+                    max_search_result_urls_to_return=self.max_urls_per_dork,
+                    proxy=proxy,
+                    verify_ssl=not self.disable_verify_ssl,
+                    verbosity=self.verbosity,
+                )
+
+                # Randomize the user agent for best results.
+                client.assign_random_user_agent()
+
+                ROOT_LOGGER.info(
+                    f"Search ( {dork_counter} / {total_dorks_to_search} ) for Google dork [ {query} ] using "
+                    f"User-Agent '{client.user_agent}' through proxy '{proxy}'"
+                )
+
+                dork_urls_list = client.search()
+
+                # Remove any exploit-db.com URLs.
+                for url in dork_urls_list:
+                    # Ignore results from specific URLs like exploit-db.com, cert.org, and GHDB's Twitter account that
+                    # may just be providing information about the vulnerability.  Keeping it simple with regex.
+                    ignore_url_list = [
+                        "https://www.kb.cert.org",
+                        "https://www.exploit-db.com/",
+                        "https://twitter.com/googlehacking/",
+                        "https://linkedin.com/",    # requires account
+                        "https://instagram.com/",   # requires account
+                    ]
+                    for ignore_url in ignore_url_list:
+                        if re.search(ignore_url, url, re.IGNORECASE):
+                            ROOT_LOGGER.warning(f"Removing {ignore_url} URL: {url}")
+                            dork_urls_list.remove(url)
+
+                dork_urls_list_size = len(dork_urls_list)
+
+                # Google dork results found.
+                if dork_urls_list:
+
+                    ROOT_LOGGER.info(f"Results: {dork_urls_list_size} URLs found for Google dork: {dork}")
+
+                    dork_urls_list_as_string = "\n".join(dork_urls_list)
+                    ROOT_LOGGER.info(f"dork_urls_list:\n{dork_urls_list_as_string}")
+
+                    self.total_urls_found += dork_urls_list_size
+
+                    # Save URLs with valid results to an .txt file.
+                    if self.save_urls_to_file:
+                        with open(f"{self.base_file_name}.txt", "a") as fh:
+                            fh.write(f"# {dork}\n")
+                            for url in dork_urls_list:
+                                fh.write(f"{url}\n")
+                            fh.write("#" * 50 + "\n")
+
+                    # fmt: off
+                    self.pydosc_results_dict["dorks"][dork] = {
+                        "urls_size": dork_urls_list_size,
+                        "urls": dork_urls_list,
+                    }
+                    # fmt: on
+
+                # No Google dork results found.
+                else:
+                    ROOT_LOGGER.info(f"Results: {dork_urls_list_size} URLs found for Google dork: {dork}")
+
+            except KeyboardInterrupt:
+                sys.exit(0)
+
+            except Exception as e:
+                ROOT_LOGGER.error(f"Error with dork: {dork}")
+                ROOT_LOGGER.error(f"EXCEPTION: {e}")
+                if type(e).__name__ == "SSLError" and (not self.disable_verify_ssl):
+                    ROOT_LOGGER.info(
+                        "If you are using self-signed certificates for an HTTPS proxy, try-rerunning with the -l "
+                        "switch to disable verifying SSL/TLS certificates.  Exiting..."
+                    )
+                    sys.exit(1)
+
+            dork_counter += 1
+
+            # Only sleep if there are more dorks to search.
+            if dork != self.google_dorks[-1]:
+                pause_time = random.choice(self.delay_between_dork_searches_list)
+                ROOT_LOGGER.info(f"Sleeping {pause_time} seconds before executing the next dork search...")
+                time.sleep(pause_time)
+
+        ROOT_LOGGER.info(
+            f"Total URLs found for the {total_dorks_to_search} total dorks searched: {self.total_urls_found}"
+        )
+
+        completion_timestamp = datetime.datetime.now().isoformat()
+
+        ROOT_LOGGER.info(f"Completion timestamp: {completion_timestamp}")
+        self.pydosc_results_dict["completion_timestamp"] = completion_timestamp
+
+        # Save pydosc_results_dict to a .json file.
+        if self.save_pydosc_results_to_json_file:
+            with open(f"{self.base_file_name}.json", "w") as fh:
+                json.dump(self.pydosc_results_dict, fh, indent=4)
+
+        return self.pydosc_results_dict
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description=f"pydosc - Passive Google Dork v{__version__}")
+    parser.add_argument(
+        "-g", dest="google_dorks_file", action="store", required=True, help="File containing Google dorks, 1 per line."
+    )
+    parser.add_argument(
+        "-d",
+        dest="domain",
+        action="store",
+        required=False,
+        help="Domain to scope the Google dork searches.  Not required.",
+    )
+    parser.add_argument(
+        "-i",
+        dest="min_delay_secs",
+        action="store",
+        required=False,
+        type=int,
+        default=37,
+        help="Minimum delay (in seconds) between a Google dork search.  Default: 37",
+    )
+    parser.add_argument(
+        "-x",
+        dest="max_delay_secs",
+        action="store",
+        required=False,
+        type=int,
+        default=60,
+        help="Maximum delay (in seconds) between a Google dork search.  Default: 60",
+    )
+    parser.add_argument(
+        "-l",
+        dest="disable_verify_ssl",
+        action="store_true",
+        required=False,
+        default=False,
+        help="Disable SSL/TLS validation.  Sometimes required if using an HTTPS proxy with self-signed certificates.",
+    )
+    parser.add_argument(
+        "-m",
+        dest="max_urls_per_dork",
+        action="store",
+        required=False,
+        type=int,
+        default=100,
+        help="Maximum results to return per dork.  Default 100.",
+    )
+    parser.add_argument(
+        "-p",
+        dest="proxies",
+        action="store",
+        required=False,
+        type=str,
+        default="",
+        help=(
+            "Comma separated string of proxies to round-robin through.  Example: "
+            "https://myproxy:8080,socks5h://127.0.0.1:9050,socks5h://127.0.0.1:9051 - The proxy scheme must confrom "
+            "per the Python requests library: https://docs.python-requests.org/en/master/user/advanced/#proxies  See "
+            "https://github.com/opsdisk/yagooglesearch for more information."
+        ),
+    )
+    parser.add_argument(
+        "-o",
+        dest="save_pydosc_results_to_json_file",
+        action="store_true",
+        required=False,
+        default=False,
+        help=(
+            "Save JSON dictionary to pydosc_results_<TIMESTAMP>.json file.  Contains more information than "
+            "pydosc_results_<TIMESTAMP>.txt"
+        ),
+    )
+    parser.add_argument(
+        "-s",
+        dest="save_urls_to_file",
+        action="store_true",
+        required=False,
+        default=False,
+        help="Save any URLS found for a dork to the pydosc_results_<TIMESTAMP>.txt file.",
+    )
+    parser.add_argument(
+        "-v",
+        dest="verbosity",
+        action="store",
+        type=int,
+        default=4,
+        help="Verbosity level (0=NOTSET, 1=CRITICAL, 2=ERROR, 3=WARNING, 4=INFO, 5=DEBUG).  Default: 4",
+    )
+
+    args = parser.parse_args()
+
+    pydosc = pydosc(**vars(args))
+    pydosc.go()
